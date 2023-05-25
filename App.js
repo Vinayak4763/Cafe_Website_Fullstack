78c05bf22894b6de9a21c9bea10ff85be3e586ca
Vinayak soni <sonivinayak716@gmail.com>
2023-05-25 20:09:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import Home from './Home'
+import About from './About.js'
+import Signup from './Signup.js'
+import Login from './Login.js'
+import Success from './Success.js'
+import Failed from './Failed.js'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import PrList from './PrList'
+function App() {
+  return (
+      <BrowserRouter>
+         
+          <Routes>
+              <Route index element={<Home />} />
+              <Route path='/PrList' element={<PrList />} />
+              <Route path='/About' element={<About />} />
+              <Route path="/signin" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/Success" element={<Success />} />
+              <Route path="/Failed" element={<Failed />} />
+          </Routes>
+        
+      </BrowserRouter>
+  );
+}
+
+export default App;
